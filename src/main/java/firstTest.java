cea016300121335e554be4f754c7d7e12526f473
zh <2786805836>
2022-10-24 18:51:28 +0800

    hotfix-first-commit

--- src/main/java/firstTest.java
+++ src/main/java/firstTest.java
@@ -1,9 +1,13 @@
 public class firstTest {
     public static void main(String[] args) {
-        System.out.println("nihao");
-        System.out.println("nihao");
-        System.out.println("nihao");
-        System.out.println("nihao");
-        System.out.println("nihao");
+        System.out.println("nihao111");
+        System.out.println("nihao222");
+        System.out.println("nihao333");
+        System.out.println("nihao444");
+        System.out.println("nihao555");
+        System.out.println("nihao666");
+        System.out.println("nihao777");
+        System.out.println("nihao888");
+        System.out.println("nihao999");
     }
 }
